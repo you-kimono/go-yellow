eb1587f425a9769a270011ec17dfdbac98bcb8ee
you-kimono <savioli.enrico@gmail.com>
2018-02-25 22:40:51 +0100

    Basic view now returns minimal HTML

--- yellowcore/views.py
+++ yellowcore/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    page = '<html><title>go-yellow</title></html>'
+    return HttpResponse(page)
